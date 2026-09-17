0f7e6ecb60337bc4413167a7f2e0496440385eae
tarathomas339-hue <tarathomas339@gmail.com>
2026-09-17 14:56:21 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -114,8 +114,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["58.210.125.162"];
+pub const RS_PUB_KEY: &str = "LALDdjsIALRwCO9KblEsCh5RUmRgi3sIPHpvrQHDNak=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
